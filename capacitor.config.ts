c0871a2c6481a166fc897bb450d7df480e1d7d35
Adithya Nanuvala <Adithyananuvala@dcmshriram.com>
2026-06-12 17:54:31 +0530

    fix: add capacitor config to git

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dcm.labnest',
+  appName: 'LabNest',
+  webDir: 'dist'
+};
+
+export default config;
